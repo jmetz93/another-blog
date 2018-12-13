e338ecc5e53ac2c2b740ac0eb3b6a1e6d2d24125
Jacob  Metzinger <jacobmetzinger@Jacobs-MacBook-Pro.local>
2018-12-13 12:21:13 -0800

    Handling registering a new user

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,3 +9,31 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    error = None
+    
+    if not username:
+      error = 'Username is required'
+    elif not password:
+      error = 'Password required'
+    elif db.execute(
+      'SELECT ID FROM user WHERE username = ?', (username,)
+    ).fetchone() is not None:
+      error = 'User {} is already registered.'.format(username)
+    
+    if error is None:
+      db.execute(
+        'INSERT INTO user (username, password) VALUES (?, ?)',
+        (username, generate_password_hash(password))
+      )
+      db.commit()
+      return redirect(url_for('auth.login'))
+
+    flash(error)
+  
+  return render_template('auth/register.html')
