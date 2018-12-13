6043d04e8e68bbb1051301723102c0cad9bbd0dd
Jacob  Metzinger <jacobmetzinger@Jacobs-MacBook-Pro.local>
2018-12-13 13:30:46 -0800

    Adding delete post functionality

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -87,3 +87,12 @@ def update(id):
             return redirect(url_for('blog.index'))
         
     render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.exectue('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
