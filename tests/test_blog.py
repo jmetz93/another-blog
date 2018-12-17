c3b81d95fb522377cba4e9322270ceb10ce6fc35
Jacob  Metzinger <jacobmetzinger@Jacobs-MacBook-Pro.local>
2018-12-17 14:01:43 -0800

    Testing coverage for the index page before logged in and after logged in

---
+++ tests/test_blog.py
@@ -0,0 +1,49 @@
+import pytest
+from app.db import get_db
+
+def test_index(client, auth):
+	response = client.get('/')
+	assert b"Log In" in response.data
+	assert b"Register" in response.data
+
+	auth.login()
+	response = client.get('/')
+	assert b'Log Out' in response.data
+	assert b'test title' in response.data
+	assert b'by test on 2018-01-01' in response.data
+	assert b'test\nbody' in response.data
+	assert b'href="/1/update"' in response.data
+
+
+@pytest.mark.parametrizer('path', (
+	'/create',
+	'/1/update',
+	'/1/delete'
+))
+def test_login_required(client, path):
+	response = client.post('path')
+	assert response.headers['Location'] == 'http://localhost/auth/login'
+
+
+def test_author_required(app, client, auth):
+	# change the post author to another user
+	with app.app_context():
+		db = get_db()
+		db.execute('UPDATE post SET author_id = 2 WHERE id = 1')
+		db.commit()
+	
+	auth.login()
+	# current user can't modify other user's post
+	assert client.post('/1/update').status_code == 403
+	assert client.post('/1/delete').status_code == 403
+	# current user doesn't see edit link
+	assert b'href="/1/update"' not in client.get('/').data
+
+
+@pytest.mark.parametrize('path', (
+	'/2/update',
+	'/2/delete',
+))
+def test_exists_required(client, auth, path):
+	auth.login()
+	assert client.post(path).status_code == 404
