1e9771a320f0c73e41e59fff1fc170420dd13294
Jacob  Metzinger <jacobmetzinger@Jacobs-MacBook-Pro.local>
2018-12-17 14:20:14 -0800

    Testing coverage for creating and updating posts

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -47,3 +47,35 @@ def test_author_required(app, client, auth):
 def test_exists_required(client, auth, path):
 	auth.login()
 	assert client.post(path).status_code == 404
+
+
+def test_create(client, auth, app):
+	auth.login()
+	assert client.get('/create').status_code == 200
+	client.post('/create', data={'title': 'created', 'body': ''})
+
+	with app.app_context():
+		db = get_db()
+		count = db.execute('SELECT COUNT(id) FROM post').fetchone()[0]
+		assert count == 2
+
+
+def test_update(client, auth, app):
+	auth.login()
+	assert client.get('/1/udpate').status_code == 200 
+	client.post('/1/update', data={'title': 'updated', 'body': ''})
+
+	with app.app_context():
+		db = get_db()
+		count = db.execute('SELECT COUNT(id) FROM post').fetchone()[0]
+		assert count == 2
+	
+
+@pytest.mark.parametrize('path', (
+	'/create',
+	'/1/update',
+))
+def test_create_update_validate(client, auth, path):
+	auth.login()
+	response = client.post(path, data={'title': '', 'body': ''})
+	assert b'Title is required.' in response.data
